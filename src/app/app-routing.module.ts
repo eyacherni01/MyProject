9d27d1efefe218710206a70629887b27dd4d0d09
eyacherni01 <cherni.eya@esprit.tn>
2026-02-12 15:30:21 +0100

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path:'', redirectTo:'home', pathMatch:'full'},
+  //{path:'home',component:HomeComponent},
+  {path:'listsugg',component:ListSuggestionComponent},
+  {path:'**', component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ListSuggestionComponent
+    ListSuggestionComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
